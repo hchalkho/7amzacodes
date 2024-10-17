6cccb02a8e6d42a9e94a3d285bc63a63bf4db559
Hamza Chalkhoune <chalkhounehamza0@gmail.com>
2024-10-17 12:22:21 +0100

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,57 @@
+// cursor animation.
+const cursort = document.querySelector('.animated-cursor');
+window.addEventListener('scroll',function(){
+    if(window.scrollY > 100){
+        cursort.style.display= 'none';
+    }
+    else{
+        cursort.style.display= 'block';
+    }
+});
+// The animated counter .
+const counters = document.querySelectorAll('.counter');
+const speed = 120;
+
+counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('data-target');
+		const count = +counter.innerText;
+
+		// Lower inc to slow and higher to slow
+		const inc = target / speed;
+
+
+		// Check if target is reached
+		if (count < target) {
+			// Add inc to count and output in counter
+			counter.innerText = Math.ceil(count + inc);
+			// Call function every ms
+			setTimeout(updateCount, 40);
+		} else {
+			counter.innerText = target;
+		}
+	}
+	// Use of the Intersection Observer.
+	const count = document.querySelector('.counter');
+    const observer = new IntersectionObserver(entries=>{
+	if(entries[0].isIntersecting){
+		updateCount();
+	}
+
+},{threshold : 1});
+observer.observe(count);
+});
+
+// About Section animation.
+const column = document.querySelectorAll('.column');
+const observer = new IntersectionObserver(entries=>{
+	entries.forEach(entry=>{
+		if(entry.isIntersecting){
+			entry.target.style.animation = 'slide 2s';
+			entry.target.style.opacity = '1';
+		}
+	})
+},{threshold : 0.8});
+column.forEach(cl =>{
+	observer.observe(cl);
+});
---
+++ app.js
@@ -0,0 +1,73 @@
+// Here we go again .
+const content = document.querySelector('.dropdown-content');
+const dropBtn = document.querySelector('.drop-btn');
+const dropBtn1 = document.querySelector('.drop-btn1');
+const dropBtn2 = document.querySelector('.drop-btn2');
+const dropIcon = document.querySelector('.dropdown i');
+/* I tried to set display none for the dropdown content in css but i didn't know why
+it doesn't work, So i did here in Javascript. */
+content.style.background = 'transparent';
+content.style.border = 'none';
+dropBtn.style.display = 'none';
+dropBtn1.style.display = 'none';
+dropBtn2.style.display = 'none';
+
+// This function is for the dropdown link (places).
+function dropdown1(){
+    content.style.background = '#fff';
+    content.style.border = '1px solid #000';
+    dropBtn.style.display = 'block';
+    dropBtn1.style.display = 'block';
+    dropBtn2.style.display = 'block';
+    dropIcon.className = 'fas fa-chevron-up';
+};
+// This function is for the dropdown link (places).
+function dropdown2(){
+    content.style.background = 'transparent';
+    content.style.border = 'none';
+    dropBtn.style.display = 'none';
+    dropBtn1.style.display = 'none';
+    dropBtn2.style.display = 'none';
+    dropIcon.className = 'fas fa-chevron-down';
+};
+// navbar in mobile view.
+const navbar = document.querySelector('nav');
+const togle = document.querySelector('.toggle button');
+const icon = document.querySelector('.toggle button i');
+togle.addEventListener('click', function(){
+    navbar.classList.toggle('actif');
+    navbar.style.top = '80px';
+    if(navbar.classList != 'navbar'){
+        icon.className = 'fas fa-times';
+    }
+    else{
+        icon.className = 'fas fa-bars';
+    }
+});
+// This removes the mobile navbar when clicking on nav-links.
+function removeNav(){
+    navbar.classList.remove('actif');
+    icon.className = 'fas fa-bars';
+}
+
+// Changing header background on scroll.
+// also showing the about section while scrolling.
+const header = document.querySelector('header');
+const about = document.getElementById('about');
+const cursor = document.getElementById('cursor');
+const darkBtn = document.querySelector('#darkMode');
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY > 80){
+        header.classList.add('sticky');
+        about.style.opacity = '1';
+        cursor.style.display = 'block';
+        darkBtn.style.display = 'block';
+    }
+    else{
+        header.classList.remove('sticky');
+        about.style.opacity = '0';
+        cursor.style.display = 'none';
+        darkBtn.style.display = 'none';
+    }
+});
